b090536ce4ae08c7b6add7ed7c459bedbd210a68
jkeepr <mail@joshua-roth.de>
2019-11-21 13:42:24 +0100

    initial commit for integration of socketio

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
 app = Flask(__name__)
+# app.config['SECRET_KEY'] = 'supersecretkey'
+socketio = SocketIO(app)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def root_route():
+    return render_template("index.html")
+
+
+@socketio.on('my_event')
+def handle_my_custom_event(json, methods=['POST']):
+    print('received my event: ' + str(json))
+    socketio.emit('response_messagee', str(json))
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True, port=8080)
--- start.sh
+++ start.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 cd ./app
-FLASK_APP=main.py \
-    FLASK_ENV=development \
-    FLASK_RUN_PORT=8080 \
-    flask run
+python main.py
