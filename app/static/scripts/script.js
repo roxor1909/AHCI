349bbdf4a7c35eafadb03d9fe67a69a966ae31a6
philenius <philenius@users.noreply.github.com>
2020-01-17 00:16:19 +0100

    show icons next to username, timer and badges

--- app/static/scripts/script.js
+++ app/static/scripts/script.js
@@ -1,12 +1,38 @@
+const PERSONAS = {
+    SENIOR: 'senior',
+    CHILD: 'child',
+    ADULT: 'adult',
+    OTHER_HUMAN: 'other human',
+    NONE: 'none'
+};
+const KNOWN_PERSONS = ['leia', 'luke', 'anakin', 'rey', 'kylo'];
+const CAMERA_WIDTH = 640;
+const CAMERA_HEIGHT = 480;
+
 let socket;
 let fabricCanvas;
-let greetingTextInstance;
+
+let userTextInstance;
+let userIconImageInstance;
+
+let gamificationBadgesIconInstance;
 let streakIconImageInstances = [];
 let streakIconText;
+
 let lightsaberImageInstance;
+
+let graphImageInstance;
+
 let clockInstance;
+
 let graphInstance;
+
 let achievementsInstance;
+
+let timerTextInstance;
+let timerIconImageInstance;
+let currentTimerValueInSeconds;
+let timerRefreshIntervalId;
 
 let lastXPositionClock = 0;
 let lastYPositionClock = 0;
@@ -16,44 +42,38 @@ let lastXPositionAchievements = 0;
 let lastYPositionAchievements = 0;
 let lastMatchedPerson;
 
-const personas = {
-    SENIOR: 'senior',
-    CHILD: 'child',
-    ADULT: 'adult',
-};
+let globalFontSize = 0;
+let globalIconWidth = 0;
 
 function establishSocketConnection() {
-    const camerWidth = 640;
-    const camerHeight = 480;
 
     // trys to connect to the host that serves the page by default
     socket = io();
 
     socket.on('connection', (socket) => {
         console.log('connected to server');
-        socket.emit('camera_frame', { camerWidth: camerWidth, camerHeight: camerHeight });
+        socket.emit('camera_frame', { camerWidth: CAMERA_WIDTH, camerHeight: CAMERA_HEIGHT });
         socket.on('disconnect', () => {
             console.log('disconnected from server');
         });
     });
 
     // get video dom element
-    const video = document.getElementById('video-canvas');
+    const videoCanvas = document.getElementById('video-canvas');
 
-    const canv = document.getElementById('recognition-canvas');
-    canv.width = camerWidth;
-    canv.height = camerHeight;
-    const ctx = canv.getContext('2d');
+    const boundingBoxCanvas = document.getElementById('recognition-canvas');
+    boundingBoxCanvas.width = CAMERA_WIDTH;
+    boundingBoxCanvas.height = CAMERA_HEIGHT;
 
     // request access to webcam
-    navigator.mediaDevices.getUserMedia({ video: { width: camerWidth, height: camerHeight } }).then((stream) => video.srcObject = stream);
+    navigator.mediaDevices.getUserMedia({ video: { width: CAMERA_WIDTH, height: CAMERA_HEIGHT } }).then((stream) => videoCanvas.srcObject = stream);
 
     // returns a frame encoded in base64
     const getFrame = () => {
         const canvas = document.createElement('canvas');
-        canvas.width = video.videoWidth;
-        canvas.height = video.videoHeight;
-        canvas.getContext('2d').drawImage(video, 0, 0);
+        canvas.width = videoCanvas.videoWidth;
+        canvas.height = videoCanvas.videoHeight;
+        canvas.getContext('2d').drawImage(videoCanvas, 0, 0);
         const data = canvas.toDataURL();
         return data;
     }
@@ -68,60 +88,141 @@ function establishSocketConnection() {
 
     socket.on('response_message', (msg) => {
         const json = JSON.parse(msg);
-        
+
         let matchedPerson = json.matchedPerson;
         if (json.matchedPerson) {
             matchedPerson = matchedPerson.toLowerCase();
         }
         const matchedPersona = assignMatchedPersonToPersona(matchedPerson);
+        console.log('person: ' + matchedPerson + '; persona: ' + matchedPersona);
 
-        showDebugMessages(json, matchedPersona);
+        if (matchedPersona === PERSONAS.OTHER_HUMAN) {
+            showDebugMessages(json, matchedPersona, videoCanvas, boundingBoxCanvas);
+        } else {
+            hideDebugMessages(videoCanvas, boundingBoxCanvas);
+        }
 
         if (matchedPerson !== lastMatchedPerson) {
-            removeStreakIcons();
-            removeUserGreeting();
-            removeLightsaber();
+            if (matchedPersona == PERSONAS.OTHER_HUMAN || matchedPersona == PERSONAS.NONE) {
+                removeGamificationBadges();
+                removeUserGreeting();
+                removeLightsaber();
+                removeGraph();
+                removeTimer();
+            } else {
+                // adaptive font size depending on persona
+                globalFontSize = (matchedPersona == PERSONAS.SENIOR) ? 80 : 40;
+                globalIconWidth = (matchedPersona == PERSONAS.SENIOR) ? 40 : 20;
 
-            showUserGreeting(matchedPerson, matchedPersona);
-            showStreakIcons(matchedPerson);
-            showLighsaber(matchedPerson);
-        }
-        lastMatchedPerson = matchedPerson;
+                removeGamificationBadges();
+                removeUserGreeting();
+                removeLightsaber();
+                removeGraph();
+                removeTimer();
 
-        json.boundingBoxes.forEach(el => {
-            if (el['class'] !== 'person') {
-                return
+                showUserGreeting(matchedPerson, matchedPersona);
+                showGamificationBadges(matchedPerson);
+                showLighsaber(matchedPerson);
+                showGraph(matchedPerson, matchedPersona);
+                showTimer();
             }
-
-            const bb = el['bounding_box'];
-            const xDist = bb['xmax'] - bb['xmin'];
-            const yDist = bb['ymax'] - bb['ymin'];
-
-            const xCenter = bb['xmin'] + xDist / 2;
-            const yCenter = bb['ymin'] + yDist / 2;
-
-            ctx.clearRect(0, 0, camerWidth, camerHeight);
-            ctx.strokeStyle = "#FFFFFF";
-            ctx.beginPath();
-            ctx.rect(bb['xmin'], bb['ymin'], xDist, yDist);
-            ctx.stroke();
-        });
-
+            lastMatchedPerson = matchedPerson;
+        }
     });
 
 }
 
+function removeTimer() {
+    if (timerRefreshIntervalId) {
+        clearInterval(timerRefreshIntervalId);
+        timerRefreshIntervalId = null;
+    }
+    if (timerTextInstance) {
+        fabricCanvas.remove(timerTextInstance);
+        timerTextInstance = null;
+    }
+    if (timerIconImageInstance) {
+        fabricCanvas.remove(timerIconImageInstance);
+        timerIconImageInstance = null;
+    }
+}
+
+function showTimer() {
+    // if timer text is defined, then the timer is already running.
+    if (timerTextInstance) {
+        return;
+    }
+
+    currentTimerValueInSeconds = 0;
+
+    // timer icon
+    let url = 'static/images/clock-solid.png';
+    fabric.Image.fromURL(url, (img) => {
+        img.set({
+            top: globalFontSize * 1.5,
+            left: 0,
+        });
+        img.scaleToWidth(globalIconWidth);
+        fabricCanvas.add(img);
+        timerIconImageInstance = img;
+    });
+
+    // timer text
+    timerRefreshIntervalId = setInterval(() => {
+        if (timerTextInstance) {
+            fabricCanvas.remove(timerTextInstance);
+        }
+
+        let seconds = (currentTimerValueInSeconds % 60).toString().split('.')[0];
+        if (seconds < 10) {
+            seconds = '0' + seconds;
+        }
+        let minutes = (currentTimerValueInSeconds / 60).toString().split('.')[0];
+        timerTextInstance = new fabric.Text(`${minutes}:${seconds}`, {
+            fontFamily: '"Courier New", Courier, monospace',
+            top: globalFontSize * 1.5,
+            left: globalIconWidth + 10,
+            fill: '#FFFFFF',
+            fontSize: globalFontSize
+        });
+        fabricCanvas.add(timerTextInstance);
+        currentTimerValueInSeconds++;
+    }, 1000);
+}
+
+function removeGraph() {
+    if (graphImageInstance) {
+        fabricCanvas.remove(graphImageInstance);
+    }
+}
+
+function showGraph(matchedPerson, matchedPersona) {
+    if (matchedPersona == PERSONAS.ADULT) {
+        let url = 'static/images/countries_grouped_by_region_and_income_groups.png';
+        fabric.Image.fromURL(url, (img) => {
+            img.set({
+                top: 400,
+                left: 0
+            });
+            img.scaleToWidth(400);
+            fabricCanvas.add(img);
+            graphImageInstance = img;
+        });
+    }
+}
+
 function assignMatchedPersonToPersona(matchedPerson) {
-    if (!matchedPerson) {
-        return undefined;
+    if (matchedPerson && matchedPerson === 'unkown') {
+        return PERSONAS.OTHER_HUMAN;
     }
     if (matchedPerson === 'anakin') {
-        return personas.CHILD;
+        return PERSONAS.CHILD;
     } else if (matchedPerson === 'rey' || matchedPerson === 'kylo') {
-        return personas.ADULT;
+        return PERSONAS.ADULT;
     } else if (matchedPerson === 'leia' || matchedPerson === 'luke') {
-        return personas.SENIOR;
+        return PERSONAS.SENIOR;
     }
+    return PERSONAS.NONE;
 }
 
 function initializeCanvas() {
@@ -174,7 +275,10 @@ function showLighsaber(matchedPerson) {
     }
 }
 
-function removeStreakIcons() {
+function removeGamificationBadges() {
+    if (gamificationBadgesIconInstance) {
+        fabricCanvas.remove(gamificationBadgesIconInstance);
+    }
     if (streakIconImageInstances.length > 0) {
         streakIconImageInstances.forEach(img => {
             fabricCanvas.remove(img);
@@ -185,74 +289,74 @@ function removeStreakIcons() {
     }
 }
 
-function showStreakIcons(matchedPerson) {
+function showGamificationBadges(matchedPerson) {
     if (matchedPerson) {
-        if (matchedPerson.toLowerCase() === 'rey' || matchedPerson.toLowerCase() === 'kylo') {
-            streakIconText = new fabric.Text('Tooth brush streak counter:', {
-                fontFamily: 'Starjedi',
-                left: 0,
-                top: 0,
-                fill: '#FFFFFF',
-                fontSize: 15
+        if (KNOWN_PERSONS.includes(matchedPerson)) {
+            fabric.Image.fromURL('static/images/star-solid.png', (img) => {
+                img.set({
+                    top: globalFontSize * 2.7,
+                    left: 0,
+                });
+                img.scaleToWidth(globalIconWidth);
+                fabricCanvas.add(img);
+                gamificationBadgesIconInstance = img;
             });
-            fabricCanvas.add(streakIconText);
         }
-        if (matchedPerson.toLowerCase() === 'kylo') {
-            for (let i = 0; i < 3; i++) {
-                const image = document.getElementById('stormtrooper');
-                let imageInstance = new fabric.Image(image, {
-                    left: 0 + i * 50,
-                    top: 20,
-                });
-                imageInstance.scaleToWidth(45);
-                fabricCanvas.add(imageInstance);
-                streakIconImageInstances.push(imageInstance);
-            }
-        } else if (matchedPerson.toLowerCase() === 'rey') {
-            for (let i = 0; i < 5; i++) {
-                const image = document.getElementById('r2d2');
-                let imageInstance = new fabric.Image(image, {
-                    left: 0 + i * 50,
-                    top: 20,
-                });
-                imageInstance.scaleToWidth(45);
-                fabricCanvas.add(imageInstance);
-                streakIconImageInstances.push(imageInstance);
-            }
+
+        // TODO: call API to retrieve badges
+
+        for (let i = 0; i < 3; i++) {
+            const image = document.getElementById('stormtrooper');
+            let imageInstance = new fabric.Image(image, {
+                top: globalFontSize * 3,
+                left: globalIconWidth + 10 + i * globalIconWidth * 1.5,
+            });
+            imageInstance.scaleToWidth(globalIconWidth);
+            fabricCanvas.add(imageInstance);
+            streakIconImageInstances.push(imageInstance);
         }
     }
 }
 
 function removeUserGreeting() {
-    if (greetingTextInstance) {
-        fabricCanvas.remove(greetingTextInstance);
+    if (userTextInstance) {
+        fabricCanvas.remove(userTextInstance);
+    }
+    if (userIconImageInstance) {
+        fabricCanvas.remove(userIconImageInstance);
     }
 }
 
 function showUserGreeting(matchedPerson, matchedPersona) {
+    fabric.Image.fromURL('static/images/user-solid.png', (img) => {
+        img.set({
+            top: 10,
+            left: 0,
+        });
+        img.scaleToWidth(globalIconWidth);
+        fabricCanvas.add(img);
+        userIconImageInstance = img;
+    });
+
     let greeting;
 
     // hide greeting when no person was recognized
     if (matchedPerson === null) {
         greeting = '';
     } else if (matchedPerson === '') {
-        greeting = getGreetingForHourOfDay() + '.';
+        greeting = 'Hello stranger.';
     } else {
-        greeting = getGreetingForHourOfDay() + ',\n' + matchedPerson.charAt(0).toUpperCase() + matchedPerson.slice(1) + '.';
+        greeting = matchedPerson.charAt(0).toUpperCase() + matchedPerson.slice(1);
     }
 
-    // adaptive font size depending on persona
-    const fontSize = (matchedPersona == personas.SENIOR) ? 80 : 40;
-    console.log(fontSize);
-
-    greetingTextInstance = new fabric.Text(greeting, {
-        fontFamily: 'Starjhol',
-        left: 10,
-        top: 800,
+    userTextInstance = new fabric.Text(greeting, {
+        fontFamily: '"Courier New", Courier, monospace',
+        top: 10,
+        left: globalIconWidth + 10,
         fill: '#FFFFFF',
-        fontSize: fontSize
+        fontSize: globalFontSize
     });
-    fabricCanvas.add(greetingTextInstance);
+    fabricCanvas.add(userTextInstance);
 }
 
 function getGreetingForHourOfDay() {
@@ -269,9 +373,41 @@ function getGreetingForHourOfDay() {
     return 'Good night';
 }
 
-function showDebugMessages(json, matchedPersona) {
+function hideDebugMessages(videoCanvas, boundingBoxCanvas) {
+    videoCanvas.style.display = 'none';
+    boundingBoxCanvas.display = 'none';
+    boundingBoxCanvas.getContext('2d').clearRect(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+    document.querySelector('#debug-text pre').style.display = 'none';
+}
+
+function showDebugMessages(json, matchedPersona, videoCanvas, boundingBoxCanvas) {
     json.matchedPersona = matchedPersona;
-    document.querySelector('#debug-text p').innerHTML = JSON.stringify(json);
+    document.querySelector('#debug-text pre').style.display = 'block';
+    document.querySelector('#debug-text pre').innerHTML = JSON.stringify(json, undefined, 2);
+
+    videoCanvas.style.display = 'block';
+    boundingBoxCanvas.display = 'block';
+
+    const boundingBoxCanvasContext = boundingBoxCanvas.getContext('2d');
+    boundingBoxCanvasContext.clearRect(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+
+    json.boundingBoxes.forEach(el => {
+        if (el['class'] !== 'person') {
+            return
+        }
+
+        const bb = el['bounding_box'];
+        const xDist = bb['xmax'] - bb['xmin'];
+        const yDist = bb['ymax'] - bb['ymin'];
+
+        const xCenter = bb['xmin'] + xDist / 2;
+        const yCenter = bb['ymin'] + yDist / 2;
+
+        boundingBoxCanvasContext.strokeStyle = "#FFFFFF";
+        boundingBoxCanvasContext.beginPath();
+        boundingBoxCanvasContext.rect(bb['xmin'], bb['ymin'], xDist, yDist);
+        boundingBoxCanvasContext.stroke();
+    });
 }
 
 /*
