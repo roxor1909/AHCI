78a2bae52736859763800ec03325086f1fc0e1f6
esel <esel@shrek.de>
2020-02-20 17:14:35 +0000

    add startup script for Rasperry Pix

---
+++ start-smart-mirror.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+docker rm -f ahci
+docker run -d --name ahci -p 8080:8080 philenius/ahci-smart-mirror-project:arm-raspi-v2
+
+sleep 60
+
+while true; do
+        echo "start chromium"
+	/usr/bin/chromium-browser --kiosk --disable-translate "http://localhost:8080" --use-fake-ui-for-media-stream --test-type --force-device-scale-factor=1.0
+        echo "chromium stopped"
+done
+
