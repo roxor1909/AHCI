f76701a6e99cb4470807861c17ad09ee869f037e
philenius <philenius@users.noreply.github.com>
2020-02-21 17:30:54 +0100

    remove loop from start-up script

--- start-smart-mirror.sh
+++ start-smart-mirror.sh
@@ -5,8 +5,6 @@ docker run -d --name ahci -p 8080:8080 philenius/ahci-smart-mirror-project:arm-r
 
 sleep 60
 
-while true; do
-        echo "start chromium"
-	/usr/bin/chromium-browser --kiosk --disable-translate "http://localhost:8080" --use-fake-ui-for-media-stream --test-type --force-device-scale-factor=1.0
-        echo "chromium stopped"
-done
+echo "start chromium"
+/usr/bin/chromium-browser --kiosk --disable-translate "http://localhost:8080" --use-fake-ui-for-media-stream --test-type --force-device-scale-factor=1.0
+echo "chromium stopped"
